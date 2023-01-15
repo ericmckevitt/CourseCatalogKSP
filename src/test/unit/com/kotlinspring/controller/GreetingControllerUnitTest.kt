be327890f213f5a0532998570800454a4b2c3a9e
Eric McKevitt <ericmckevitt@Erics-MacBook-Pro-2.local>
2023-01-15 01:42:52 -0700

    Mocked GreetingService in GreetingController unit test

---
+++ src/test/unit/com/kotlinspring/controller/GreetingControllerUnitTest.kt
@@ -0,0 +1,42 @@
+package com.kotlinspring.controller
+
+import com.kotlinspring.service.GreetingService
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@WebMvcTest(controllers = [GreetingController::class])
+@AutoConfigureWebTestClient
+class GreetingControllerUnitTest {
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @MockkBean
+    lateinit var greetingServiceMock: GreetingService
+
+    @Test
+    fun retrieveGreeting() {
+
+        val name = "Eric"
+
+        // Mock the service call so that it always returns the same value
+        every { greetingServiceMock.retrieveGreeting(any()) } returns "$name, Hello from default profile"
+
+        // Given a GET req @ uri, expect 2xx return and String body, store the body in result
+        val result = webTestClient.get()
+            .uri("/v1/greetings/{name}", name)
+            .exchange()
+            .expectStatus().is2xxSuccessful
+            .expectBody(String::class.java)
+            .returnResult()
+
+        // Assert that expected outcome is what was returned from GET req
+        Assertions.assertEquals("$name, Hello from default profile", result.responseBody)
+    }
+}
