7989d430fad7da2096b7f4aa2628ef147da0b0c7
Eric McKevitt <ericmckevitt@Erics-MacBook-Pro-2.local>
2023-01-15 15:29:09 -0700

    POST endpoint for creating courses

---
+++ src/main/kotlin/com/kotlinspring/service/CourseService.kt
@@ -0,0 +1,28 @@
+package com.kotlinspring.service
+
+import com.kotlinspring.dto.CourseDTO
+import com.kotlinspring.entity.Course
+import com.kotlinspring.repository.CourseRepository
+import mu.KLogging
+import org.springframework.stereotype.Service
+
+@Service
+class CourseService(val courseRepository: CourseRepository) {
+
+    companion object: KLogging()
+
+    fun addCourse(courseDTO: CourseDTO): CourseDTO {
+        // convert from courseDTO to course Entity
+        val courseEntity = courseDTO.let {
+            Course(null, it.name, it.category)
+        }
+
+        courseRepository.save(courseEntity)
+
+        logger.info("Saved course is: $courseEntity")
+
+        return courseEntity.let {
+            CourseDTO(it.id, it.name, it.category)
+        }
+    }
+}
