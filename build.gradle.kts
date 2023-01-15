9b220acb52f7dfeda64a0be9e7d68c5f26860978
Eric McKevitt <ericmckevitt@Erics-MacBook-Pro-2.local>
2023-01-15 01:32:51 -0700

    Integration test for GreetingController

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
 	// runtimeOnly("org.postgresql:postgresql")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webflux")
 }
 
 tasks.withType<KotlinCompile> {
